49169758b5c184212e8d471bd0d9d8e2c741ea60
UwU <iishumakov@gmail.com>
2023-12-08 18:01:03 +0300

    Another proto

--- Ciphers/BlockCipherParallel/Feistel.cu
+++ Ciphers/BlockCipherParallel/Feistel.cu
@@ -9,8 +9,35 @@ __device__ __host__ unsigned ceilDiv(T1 Lhs, T2 Rhs) {
   return ceil(LhsF / RhsF);
 }
 
-void makeRound(unsigned char *Block, unsigned BlockSize) {
+void f(unsigned char *Block, unsigned char *OutBlock,
+       unsigned BlockSize,
+       unsigned char *SubKey, unsigned SubKeySize) {
+  for (unsigned i = 0; i < BlockSize / 2; i++) {
+    uint8_t currHBByte = Block[i];
+    uint8_t currKeyByte = SubKey[i % SubKeySize];
 
+    // Some weird stuff is going on here
+    OutBlock[i] = (currHBByte ^ currKeyByte & currHBByte ^ 0xD5) & 
+                  (currKeyByte >> 3) ^ currHBByte | currKeyByte;
+  }
+}
+
+void makeRound(unsigned char *Block, unsigned BlockSize, 
+               unsigned char *SubKey, unsigned SubKeySize) {
+  unsigned char *Lhs = Block;
+  unsigned char *Rhs = Block + BlockSize / 2;
+
+  unsigned char *OldRhs = (unsigned char*)malloc(BlockSize / 2);
+  for (unsigned i = 0; i < BlockSize / 2; i++)
+    OldRhs[i] = Rhs[i];
+
+  f(Rhs, Rhs, BlockSize, SubKey, SubKeySize);
+
+  for (unsigned i = 0; i < BlockSize / 2; i++)
+    Rhs[i] = Lhs[i] ^ Rhs[i];
+
+  for (unsigned i = 0; i < BlockSize / 2; i++)
+    Lhs[i] = OldRhs[i];
 }
 
 void swapBlockHalfs(unsigned char *Block, unsigned BlockSize) {
@@ -23,7 +50,9 @@ void encryptKernel(EncryptData Data) {
   unsigned NumOfBlocks = ceilDiv(Data.Size, BlockSizeInBytes);
   unsigned ThreadID = blockIdx.x * blockDim.x + threadIdx.x;
   for (unsigned i = ThreadID; i < NumOfBlocks; i += Data.NumOfThreads) {
-    makeRound(Data.CudaDataPtr +  i * BlockSizeInBytes, BlockSizeInBytes);
+    for (unsigned i = 0; i < Data.NumberOfRound; ++i)
+      makeRound(Data.CudaDataPtr +  i * BlockSizeInBytes, BlockSizeInBytes,
+                Data.Subkeys, Data.SubKeySizes);
     swapBlockHalfs(Data.CudaDataPtr +  i * BlockSizeInBytes, BlockSizeInBytes);
   }
 }
--- Ciphers/BlockCipherParallel/Feistel.cu.h
+++ Ciphers/BlockCipherParallel/Feistel.cu.h
@@ -22,17 +22,30 @@ struct EncryptData {
   unsigned NumOfThreads = 1;
 
   unsigned CipherBlockBitSize = 64;
+  unsigned NumberOfRound = 16;
   unsigned char *CudaDataPtr;
   unsigned Size = 0;
+
+  unsigned char *SubKey;
+  unsigned SubKeySize;
 };
+
+static constexpr unsigned KeyByteSize = 8;
+
+using Key = unsigned char[KeyByteSize];
 
 void encryptOnCuda(EncryptData Data);
 
 template <int BlockSize, int NumOfThreads>
 class FeistelCudaEncryptor {
   static constexpr size_t BlockBitSize = 64; 
+  std::array<unsigned char> MasterKey = {14, 48, 228, 13, 37, 42, 69, 223};
+  std::vector<Key> SubKey;
   
 public:
+  FeistelCudaEncryptor() {
+    
+  }
 
   template <typename Cont_t, typename Insert>
   void encrypt(const Cont_t &Cont, Insert Inserter) {
@@ -48,6 +61,9 @@ public:
     Data.CipherBlockBitSize = BlockBitSize;
     Data.CudaDataPtr = CudaDataPtr;
     Data.Size = Cont.size();
+    CUDA_CHECK(cudaMalloc((void **)&Data.SubKey, SubKey.size()));
+    CUDA_CHECK(cudaMemcpy(Data.SubKey, SubKey.data(), SubKey.size(),
+                          cudaMemcpyHostToDevice));
     encryptOnCuda(Data);
 
     std::vector<typename Cont_t::value_type> ResData(Cont.size());
